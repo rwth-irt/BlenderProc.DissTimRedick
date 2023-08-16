1e9bfd82d98f5892fcdcf473cb3c027003b7c5c2
Tim Redick <t.redick@irt.rwth-aachen.de>
2023-08-16 10:11:57 +0200

    Update remesh script

--- remesh_models_for_eval.py
+++ remesh_models_for_eval.py
@@ -1,22 +1,11 @@
-# Author: Tomas Hodan (hodantom@cmp.felk.cvut.cz)
-# Center for Machine Perception, Czech Technical University in Prague
-
-"""'Uniformly' resamples and decimates 3D object models for evaluation.
-
-Note: Models of some T-LESS objects were processed by Blender (using the Remesh modifier).
-"""
-
-import os
 import numpy as np
+import os
 import open3d as o3d
 
 models_in_path = "/home/rd/code/BlenderProc/bop_datasets/steri/models_cad"
 models_out_path = "/home/rd/code/BlenderProc/bop_datasets/steri/models_eval"
 if not os.path.exists(models_out_path):
     os.makedirs(models_out_path)
-
-# Path to scripts/meshlab_scripts/remesh_for_eval.mlx.
-meshlab_script_path = "remesh_for_eval_cell=0.25.mlx"
 
 # Process models of all objects in the selected dataset.
 # Iterate over all files in models_path and enumerate the ids
@@ -27,18 +16,32 @@ for obj_id, file_name in enumerate(os.listdir(models_in_path)):
 
     mesh = o3d.io.read_triangle_mesh(model_in_path)
     # Preprocessing
-    mesh.remove_unreferenced_vertices()
-    mesh.remove_duplicated_vertices()
-    mesh.remove_degenerate_triangles()
-    mesh.compute_vertex_normals()
+    mesh = mesh.remove_unreferenced_vertices()
+    mesh = mesh.remove_duplicated_vertices()
+    mesh = mesh.remove_degenerate_triangles()
+    mesh = mesh.compute_triangle_normals()
+    mesh = mesh.compute_vertex_normals()
+
     # Resampling
-    n_vertices = int(1.0 * len(mesh.vertices))
-    points = mesh.sample_points_uniformly(n_vertices)
-    radii = [1, 2, 5, 10, 20, 50]
+    n_vertices = 100_000
+    points = mesh.sample_points_uniformly(n_vertices, use_triangle_normal=True)
+    radii = o3d.utility.DoubleVector([0.1, 1, 10, 50, 200])
     rec_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(
-        points, o3d.utility.DoubleVector(radii)
+        points, radii
     )
-    # o3d.visualization.draw_geometries([rec_mesh])
+
+    # Voxel simplification to reduce computational overhead
+    min_bbox = mesh.get_minimal_oriented_bounding_box()
+    diameter = np.linalg.norm(
+        np.asarray(min_bbox.get_max_bound()) - np.asarray(min_bbox.get_min_bound())
+    )
+    voxel_size = 0.25 / 100 * diameter
+    rec_mesh = rec_mesh.simplify_vertex_clustering(voxel_size)
+
+    rec_mesh = rec_mesh.compute_vertex_normals()
+    rec_mesh = rec_mesh.compute_triangle_normals()
+    o3d.visualization.draw_geometries([rec_mesh], mesh_show_wireframe=True)
+
     o3d.io.write_triangle_mesh(model_out_path, rec_mesh)
 
 print("Done.")
