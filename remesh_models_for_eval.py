dc0fdbc1a2e63dde20e6d6410d64471315b8364e
Tim Redick <t.redick@irt.rwth-aachen.de>
2023-08-16 11:29:01 +0200

    Improve remeshing

--- remesh_models_for_eval.py
+++ remesh_models_for_eval.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import open3d as o3d
+import shutil
 
 models_in_path = "bop_datasets/steri/models_cad"
 models_out_path = "bop_datasets/steri/models_eval"
@@ -10,11 +11,21 @@ if not os.path.exists(models_out_path):
 # Process models of all objects in the selected dataset.
 # Iterate over all files in models_path and enumerate the ids
 for obj_id, file_name in enumerate(os.listdir(models_in_path)):
-    print("Processing object {}".format(file_name))
     model_in_path = os.path.join(models_in_path, file_name)
     model_out_path = os.path.join(models_out_path, file_name)
+    # Only process ply files
+    if not file_name.endswith("ply"):
+        print("Copy non *.ply file {}".format(file_name))
+        # copy the file to the output folder
+        shutil.copyfile(model_in_path, model_out_path)
+        continue
 
+    # Do not remesh if out file already exists
+    if os.path.exists(model_out_path):
+        continue
     mesh = o3d.io.read_triangle_mesh(model_in_path)
+
+    print("Processing object {}".format(file_name))
     # Preprocessing
     mesh = mesh.remove_unreferenced_vertices()
     mesh = mesh.remove_duplicated_vertices()
@@ -22,25 +33,32 @@ for obj_id, file_name in enumerate(os.listdir(models_in_path)):
     mesh = mesh.compute_triangle_normals()
     mesh = mesh.compute_vertex_normals()
 
-    # Resampling
-    n_vertices = 100_000
-    points = mesh.sample_points_uniformly(n_vertices, use_triangle_normal=True)
-    radii = o3d.utility.DoubleVector([0.1, 0.5, 1, 5, 50])
-    rec_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(
-        points, radii
-    )
-
-    # Voxel simplification to reduce computational overhead
+    # Calc diameter for mesh density. 0.5% is not suitable for thin instrument
     min_bbox = mesh.get_minimal_oriented_bounding_box()
     diameter = np.linalg.norm(
         np.asarray(min_bbox.get_max_bound()) - np.asarray(min_bbox.get_min_bound())
     )
-    voxel_size = 0.25 / 100 * diameter
-    rec_mesh = rec_mesh.simplify_vertex_clustering(voxel_size)
+    mesh_density = 0.25 / 100 * diameter
 
+    # Resampling
+    n_vertices = 300_000
+    points = mesh.sample_points_uniformly(n_vertices, use_triangle_normal=True)
+    rec_mesh, _ = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
+        points, width=mesh_density
+    )
+
+    # Set normals and color for visualization
     rec_mesh = rec_mesh.compute_vertex_normals()
     rec_mesh = rec_mesh.compute_triangle_normals()
-    o3d.visualization.draw_geometries([rec_mesh], mesh_show_wireframe=True)
+    rec_mesh.paint_uniform_color(np.asarray([0.9, 0.9, 1]))
+
+    # Visually inspect
+    # o3d.visualization.draw_geometries(
+    #     [rec_mesh],
+    #     mesh_show_wireframe=True,
+    #     mesh_show_back_face=True,
+    #     window_name=file_name,
+    # )
 
     o3d.io.write_triangle_mesh(model_out_path, rec_mesh)
 
